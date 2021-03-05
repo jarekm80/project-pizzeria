f920b2bd364f62a7251ccd1d51817a2ebadb0783
Jarek Matwijewicz <alterro@gmail.com>
2021-03-05 18:12:22 +0100

    finish initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -74,26 +74,25 @@
     }
 
 
-  initAccordion(){
-    const thisProduct = this;
-
-    /* [DONE] find the clickable trigger (the element that should react to clicking) */
-
-    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-    console.log('cT : ', clickableTrigger);
-    /* START: add event listener to clickable trigger on event click */
-//    clickableTrigger.addEventListener('click', function(event) {
-      /* [DONE] prevent default action for event */
-//      event.preventDefault();
-      /* find active product (product that has active class) */
-
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-
-      /* toggle active class on thisProduct.element */
-//    });
-
+    initAccordion(){
+      const thisProduct = this;
+      /* [DONE] find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* [DONE] prevent default action for event */
+        event.preventDefault();
+        /* [DONE] find active product (product that has active class) */
+        let activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        /* [DONE] if there is active product and it's not thisProduct.element, remove class active from it */
+        if ((activeProduct != null) && (activeProduct != thisProduct.element)){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* [DONE] toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+    }
   }
-}
 
   const app = {
     initMenu: function(){
